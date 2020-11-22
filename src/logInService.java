e3a1306335ce0fe53ebbcc392a49894e729a69b8
Ghoya <canadamubarak1@gmail.com>
2020-11-21 23:53:20 -0800

    added to service

--- src/logInService.java
+++ src/logInService.java
@@ -1,3 +1,4 @@
 public class logInService {
 // line1 added
+    // added god words to Login Service
 }
--- src/logInUtility.java
+++ src/logInUtility.java
@@ -1,4 +1,6 @@
 public class logInUtility {
-
+    public static void main(String[] args) {
+    //    line 3 added
+    }
     // line2 added
 }
